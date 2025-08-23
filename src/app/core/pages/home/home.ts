c025bf4c1b5b4aac27f01f230508fbfb63c4a342
Cesar- <legende.e@hotmail.com>
2025-08-22 19:37:35 -0500

    feat: refactor home page layout and styles for improved responsiveness and design

--- src/app/core/pages/home/home.ts
+++ src/app/core/pages/home/home.ts
@@ -5,7 +5,6 @@ import { RouterModule  } from '@angular/router';
   selector: 'app-home',
   imports: [RouterModule],
   templateUrl: './home.html',
-  styleUrl: './home.css'
 })
 export class Home  {
   
