1ea922350af933875d0277d870733e272098dad0
Cesar- <legende.e@hotmail.com>
2025-08-23 00:42:40 -0500

    feat: add team members section with dynamic data and improved layout

--- src/app/core/pages/about/about.ts
+++ src/app/core/pages/about/about.ts
@@ -1,43 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TEAM_MEMBERS } from '../../consts/about/members.const';
 
 @Component({
   selector: 'app-about',
   imports: [CommonModule],
   templateUrl: './about.html',
-  styleUrl: './about.css'
 })
 export class About {
-    integrantes = [
-    {
-      nombre: 'César Aróstegui',
-      descripcion: 'Estudiante de Ingeniería de Software',
-      imagen: 'members/cesar.png',
-    },
-    {
-      nombre: 'Rodrigo Solano',
-      descripcion: 'Estudiante de Ingeniería de Sistemas',
-      imagen: 'members/rodrigo.png',
-    },
-    {
-      nombre: 'Ángel Crispín',
-      descripcion: 'Estudiante de Ingeniería de Software',
-      imagen: 'members/angel.png',
-    },
-    {
-      nombre: 'Alexander Montoya',
-      descripcion: 'Estudiante de Ingeniería de Software',
-      imagen: 'members/montoya.png',
-    },
-    {
-      nombre: 'Giordano Trejo',
-      descripcion: 'Estudiante de Ingeniería de Software',
-      imagen: 'members/giordano.png',
-    },
-    {
-      nombre: 'Josep Melgarejo',
-      descripcion: 'Estudiante de Ingeniería de Software',
-      imagen: 'members/melgarejo.png',
-    },
-  ];
+  teamMembers = TEAM_MEMBERS;
 }
